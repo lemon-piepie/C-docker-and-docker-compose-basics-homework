5591447ed526cd25055233995422967990d5fe69
lemon-piepie <purplechilde@163.com>
2020-09-23 10:30:05 +0800

    create api file

--- alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceApplication.java
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceApplication.java
@@ -2,21 +2,12 @@ package com.thoughtworks.capability.gtb.alice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@RestController
 public class AliceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AliceApplication.class, args);
   }
 
-  @GetMapping("/hello")
-  public String helloFromBob(){
-    RestTemplate restTemplate = new RestTemplate();
-    return restTemplate.getForObject("http://bob:8081/hello", String.class);
-  }
 }
